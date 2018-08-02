245fb5111a7ed5de10ea4186083ee8d09ff8e726
Vilde Vevatne <v-viveva@microsoft.com>
2018-08-02 12:01:31 -0700

    Add employee class

---
+++ Abstraction/Abstraction/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public abstract class Employee
+    {
+        public string Name { get; private set; }
+
+        public Employee(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public abstract int GetSalary();
+    }
+}
---
+++ Abstraction/Abstraction/WorkerBee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public class WorkerBee : Employee
+    {
+        public WorkerBee(string Name): base(name)
+    }
+}
